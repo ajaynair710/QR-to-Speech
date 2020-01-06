9ea5a71ba3a3a9caf73255a8468a937fcea1015e
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2020-01-06 23:12:30 +0530

    initial

---
+++ app/src/main/java/com/aj/qr/MainActivity.java
@@ -0,0 +1,35 @@
+package com.aj.qr;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static TextView tvresult;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvresult = (TextView)findViewById(R.id.tvresult);
+
+        btn = (Button)findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
